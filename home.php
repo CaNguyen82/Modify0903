cbff51bb2c29989113b1d0079ea76485e425f3c3
CaNguyen82 <cants@evolableasia.vn>
2015-03-09 16:04:11 +0700

    Up new
    Up

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Keywords" content="MARGINAL#4,マージナルナンバーフォー,ラグランジュポイント,LAGRANGE POINT,UNICORN Jr.,ユニコーンジュニア,リジェット,Rejet," />
+<meta name="description" content="銀河の果てまでKISSを届けるアイドル！「MARGINAL#4」、太陽から月まで愛を囁くアイドル！「LAGRANGE POINT」、空より降り立った若き一角獣！「UNICORN Jr.」総合TOPです" />
+<title>「MARGINAL#4／LAGRANGE POINT／UNICORN Jr.」Official Site 総合TOP</title>
+<link type="text/css" rel="stylesheet" href="unicorn/css/style.css" />
+<script type="text/javascript" src="unicorn/common/js/jquery-1.7.2.min.js"></script>
+<!--[if lte IE 9]>
+<script type="text/javascript" src="../common/js/html5shiv.js"></script>
+<![endif]-->
+<script type="text/javascript" src="unicorn/common/js/jquery.belatedPNG.js"></script>
+<script type="text/javascript" src="unicorn/common/js/jscrollpane/jquery.jscrollpane.min.js"></script>
+<script type="text/javascript" src="unicorn/common/js/jscrollpane/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="unicorn/common/js/jquery.jplayer.js"></script>
+<script type="text/javascript" src="unicorn/common/js/fancybox/jquery.fancybox.js"></script>
+<script type="text/javascript" src="unicorn/common/js/jquery.rollover.js"></script>
+<script type="text/javascript" src="unicorn/common/js/flexcroll/flexcroll.js"></script>
+<script type="text/javascript" src="unicorn/common/js/jquery.socialbutton.js"></script>
+<script type="text/javascript" src="unicorn/common/js/script.js"></script>
+<script type="text/javascript" src="unicorn/common/js/include.js"></script>
+</head>
+
+<body id="MainIndex" class="homePage">
+<div id="fb-root"></div>
+<div id="page">
+<div class="wrapper">
+<header id="">
+	<div class="inner">
+		<h1> <img src="unicorn/img/home/img_logo.png" alt="" /></h1>
+	</div>
+</header>
+<div id="GlobalContents">
+	<div id="home_mx">
+		<p class="taC t50b0"> <a href="marginal/"> <img src="unicorn/img/home/btn_01.png" alt="" class="opa" /> </a> </p>
+		<p class="taC t50b0"> <a href="lagrange_point/"> <img src="unicorn/img/home/btn_02.png" alt="" class="opa" /> </a> </p>
+		<p class="taC t50b0"> <a href="unicorn/"> <img src="unicorn/img/home/btn_03.png" alt="" class="opa" /> </a> </p>
+	</div>
+</div>
+<footer id="GlobalFooter">
+<div class="inner">
+	<div class="fback">
+		<div class="whatsnew"> <img src="unicorn/common/img/footer/whatsnew.png" alt="What's New" class="" /> </div>
+		<?php //include( 'unicorn/libs/footer.php'); ?>
+		</footer>
+	</div>
+</div>
+<div id="sv20a"></div>
+</body>
+</html>
